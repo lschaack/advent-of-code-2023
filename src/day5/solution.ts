37f139043434a0d04eca16f23f49cd71a7a467bf
Luke Schaack <luke.schaack@gmail.com>
2023-12-11 01:14:33 -0800

    reach my personal limit for the range-based approach...I have another idea

--- src/day5/solution.ts
+++ src/day5/solution.ts
@@ -44,64 +44,50 @@ type MapType =
   | 'temperatureToHumidity'
   | 'humidityToLocation';
 
-type Range = {
-  sourceStart: number;
-  destinationStart: number;
-  rangeLength: number;
-  sourceRange: [number, number];
-  destinationRange: [number, number];
+type Range = [number, number];
+type BinaryRangeOperator<ReturnType> = (left: Range, right: Range) => ReturnType;
+type RangeRelationship = 'no-overlap' | 'partly-contained-lower' | 'contained' | 'partly-contained-upper' | 'contains';
+
+type Transform = {
+  sourceRange: Range;
+  destinationRange: Range;
   shift: number;
 }
 
 type Almanac = {
   seeds: number[];
-  maps: Range[][];
+  transforms: Transform[][];
+  precomputedTransform: Transform;
 };
 
 // thank you multi-cursor editing
 const matchAlmanac = /^seeds:(?<seeds>[\d\s]+)seed-to-soil map:(?<seedToSoil>[\d\s]+)soil-to-fertilizer map:(?<soilToFertilizer>[\d\s]+)fertilizer-to-water map:(?<fertilizerToWater>[\d\s]+)water-to-light map:(?<waterToLight>[\d\s]+)light-to-temperature map:(?<lightToTemperature>[\d\s]+)temperature-to-humidity map:(?<temperatureToHumidity>[\d\s]+)humidity-to-location map:(?<humidityToLocation>[\d\s]+)$/;
 
-const parseRangeSource = (source: string): Range => {
+const parseRangeSource = (source: string): Transform => {
   const [destinationStart, sourceStart, rangeLength] = source
     .split(' ')
     .map(rangeVal => parseInt(rangeVal));
 
   // TODO: should probably convert start + range into actual bounds
   return {
-    sourceStart,
-    destinationStart,
-    rangeLength,
     sourceRange: [sourceStart, sourceStart + rangeLength],
     destinationRange: [destinationStart, destinationStart + rangeLength],
     shift: destinationStart - sourceStart,
   };
 }
 
-const parseMapSource = (source: string): Array<Range> => {
+const parseMapSource = (source: string): Array<Transform> => {
   return source
     .trim()
     .split(/\n+/)
     .map(parseRangeSource);
 }
 
-const parseAlmanacSource = (source: string): Almanac => {
-  const {
-    groups: { seeds, ...maps }
-  } = source.match(matchAlmanac);
-
-  return {
-    seeds: seeds.trim().split(/\s+/).map(seed => parseInt(seed)),
-    maps: Object
-      .values(maps)
-      .map(parseMapSource),
-  }
-}
-
-const isInRange = (x: number, [lowerBound, upperBound]: [number, number]) => {
+const isInRange = (x: number, [lowerBound, upperBound]: Range) => {
   return x >= lowerBound && x <= upperBound;
 }
 
-const filterSeed = (seed: number, maps: Almanac['maps']) => {
+const filterSeed = (seed: number, maps: Almanac['transforms']) => {
   return maps.reduce((position, map) => {
     for (const filter of map) {
       if (isInRange(position, filter.sourceRange)) {
@@ -115,15 +101,180 @@ const filterSeed = (seed: number, maps: Almanac['maps']) => {
 
 export const solvePartOne = () => {
   const almanacSource = input;
-
   const almanac = parseAlmanacSource(almanacSource);
-
-  const locations = almanac.seeds
-    .map(seed => filterSeed(seed, almanac.maps))
+  const locations = almanac.seeds.map(seed => filterSeed(seed, almanac.transforms))
 
   return locations.reduce(minimize);
-  // return JSON.stringify(almanac, null, 2);
 };
+
+const compare: BinaryRangeOperator<RangeRelationship> = ([lowerA, upperA], [lowerB, upperB]) => {
+  if (upperA < lowerB) return 'no-overlap';
+  else if (upperA <= upperB)
+    if (lowerA <= lowerB) return 'partly-contained-lower';
+    else return 'contained';
+  else if (lowerA <= upperB)
+    if (lowerA > lowerB) return 'partly-contained-upper';
+    else return 'contains';
+  else return null;
+}
+
+const intersect: BinaryRangeOperator<Range | null> = (a, b, relationship = compare(a, b)) => {
+  const [lowerA, upperA] = a;
+  const [lowerB, upperB] = b;
+
+  switch(relationship) {
+    case 'no-overlap': return null;
+    case 'partly-contained-lower': return [lowerB, upperA];
+    case 'contained': return [lowerA, upperA];
+    case 'partly-contained-upper': return [lowerA, upperB];
+    case 'contains': return [lowerB, upperB];
+  }
+}
+
+const union: BinaryRangeOperator<Range | null> = (a, b, relationship = compare(a, b)) => {
+  const [lowerA, upperA] = a;
+  const [lowerB, upperB] = b;
+
+  switch(relationship) {
+    case 'no-overlap': return null;
+    case 'partly-contained-lower': return [lowerA, upperB];
+    case 'contained': return [lowerB, upperB];
+    case 'partly-contained-upper': return [lowerB, upperA];
+    case 'contains': return [lowerA, upperA];
+  }
+}
+
+const except: BinaryRangeOperator<Range[] | null> = (a, b, relationship = compare(a, b)) => {
+  const [lowerA, upperA] = a;
+  const [lowerB, upperB] = b;
+
+  switch(relationship) {
+    case 'no-overlap': return [b];
+    case 'partly-contained-lower': return [[lowerA, lowerB - 1]];
+    case 'contained': return null;
+    case 'partly-contained-upper': return [[upperB + 1, upperA]];
+    case 'contains': return [[lowerA, lowerB - 1], [upperB + 1, upperB]];
+  }
+}
+
+type Overlaps = {
+  a: Range[] | null;
+  b: Range[] | null;
+  both: Range | null;
+}
+const getOverlaps: BinaryRangeOperator<Overlaps> = (a, b) => {
+  return {
+    a: except(a, b),
+    b: except(b, a),
+    both: intersect(a, b),
+  }
+}
+
+const doTransformsOverlap = (a: Transform, b: Transform): boolean => {
+  return compare(a.destinationRange, b.sourceRange) !== 'no-overlap';
+}
+
+const injectAtIndex = <T>(toInject: T[], sourceArray: T[], index: number) => {
+  return [
+    ...sourceArray.slice(0, index),
+    ...toInject,
+    ...sourceArray.slice(index  + 1),
+  ]
+}
+
+// run a set of ranges through another
+// seed is always one range, but as [a]
+// "filter" a set of input ranges through another, expanding each if 
+const breakUpTransformRanges = (input: Transform[], filterTransforms: Transform[]): Transform[] => {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < filterTransforms.length; j++ ) {
+      const a = input[i];
+      const b = filterTransforms[j];
+
+      if (doTransformsOverlap(a, b)) {
+        return breakUpTransformRanges(
+          ,
+          filterTransforms
+        )
+      }
+    }
+  }
+    const a of input) {
+    for (const b of filterTransforms) {
+      if (doTransformsOverlap(a, b)) {
+        // splice a to include all of input before and after the overlap, and then spread the overlap in the place of a
+
+        return breakUpTransformRanges([
+          ...a.splice()
+          ...breakUpTransformRanges(),
+          ...,
+          
+        ]);
+      }
+    }
+  }
+}
+
+const collapseTransforms = (transformMaps: Transform[][]): Transform[] => {
+  return transformMaps.reduce<Transform[]>((collapsedTransform, transformMap) => {
+    // compare each range in `collapsedTransform` compare to each in `transform`,
+    // combining overlapping transforms by breaking up their ranges into separate
+    // transforms which apply one transform, the other, or both
+    return breakUpTransformRanges(collapsedTransform, transformMap);
+
+    // TODO: what about transforms that have no overlap? i.e. new ranges in B
+    collapsedTransform.flatMap(a => {
+      // here, this should be a reduce
+      // run the whole of [a] through a reduce so that it can "expand" in response to each newly transform range
+      // or make it recursive?
+      // start w/both of these sorted
+      return breakUpTransformRanges(a, transformMap)
+
+      return transformMap.reduce((expandedTransforms, b) => {
+        if (doTransformsOverlap(a, b)) {
+          const {
+            a: applyA,
+            b: applyB,
+            both: applyBoth
+          } = getOverlaps(a.destinationRange, b.sourceRange);
+
+          const createTransform = (sourceRange: Range, shift: number): Transform => ({
+            sourceRange,
+            destinationRange: [sourceRange[0] + shift, sourceRange[1] + shift],
+            shift,
+          })
+
+          const splitTransforms: Transform[] = [
+            ...applyA.map(range => createTransform(range, a.shift)),
+            ...applyB.map(range => createTransform(range, b.shift)),
+            createTransform(applyBoth, a.shift + b.shift),
+          ].filter(Boolean);
+
+          return splitTransforms;
+        } else {
+          return a;
+        }
+      }, [a])
+    })
+  }, []);
+}
+
+const parseAlmanacSource = (source: string): Almanac => {
+  const {
+    groups: { seeds, ...rawMaps }
+  } = source.match(matchAlmanac);
+
+  const maps = Object
+    .values(rawMaps)
+    .map(parseMapSource);
+
+  const seedFilter = collapseTransforms(maps);
+
+  return {
+    seeds: seeds.trim().split(/\s+/).map(seed => parseInt(seed)),
+    transforms: maps,
+  }
+}
 
 export const solvePartTwo = () => {
   const almanacSource = input;
@@ -137,12 +288,8 @@ export const solvePartTwo = () => {
 
   let locations = [];
   for (const [seedStart, rangeLength] of seedPairs) {
-    const nSeeds = seedStart + rangeLength;
-
     for (let seed = seedStart; seed < seedStart + rangeLength; seed++) {
-      console.log('starting work on seed', seed, '/', nSeeds)
-
-      locations.push(filterSeed(seed, almanac.maps));
+      locations.push(filterSeed(seed, almanac.transforms));
     }
   }
 
